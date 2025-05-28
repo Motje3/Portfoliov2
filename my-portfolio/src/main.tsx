8bd6dd82a7d9793a903b57078d3828adc34f922e
Motje <Mohamaadflaha2014@gmail.com>
2025-05-28 23:45:45 +0200

    NEW MAIN

--- my-portfolio/src/main.tsx
+++ my-portfolio/src/main.tsx
@@ -1,18 +1,43 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Outlet,
+  ScrollRestoration,
+} from "react-router-dom";
 import "./index.css";
 
 import Portfolio from "./Portfolio.tsx";
-import Project1Page from "./pages/Project-1.tsx"; 
+import Project1Page from "./pages/Project-1.tsx";
+
+const RootLayout = () => {
+  return (
+    <>
+      <ScrollRestoration />
+      <Outlet />
+    </>
+  );
+};
+
+const router = createBrowserRouter([
+  {
+    element: <RootLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Portfolio />,
+      },
+      {
+        path: "/project-1",
+        element: <Project1Page />,
+      },
+    ],
+  },
+]);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Portfolio />} />
-        <Route path="/project-1" element={<Project1Page />} />
-      </Routes>
-    </BrowserRouter>
+    <RouterProvider router={router} />
   </StrictMode>
 );
